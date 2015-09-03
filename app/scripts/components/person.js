aed250ab507e4ccc6a8c5030e7e5bf62b47c66c4
Maxim <urukov-maxim@mail.ru>
2015-09-03 20:47:21 +0300

    верстка сотрудника

--- app/scripts/components/person.js
+++ app/scripts/components/person.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Component, PropTypes} from 'react';
 
+const b = 'people-suggest';
+
 export default class Person extends Component {
 
     static propTypes = {
@@ -8,11 +10,36 @@ export default class Person extends Component {
     };
 
     render() {
+
         const {person} = this.props;
 
         return (
-            <div className='person'>
-                <div className="person__name">{person.name}</div>
+            <div className={`${b}__employee ${b}__employee_id_${person.id}`}>
+
+                <div className={`${b}__avatar`}>
+                    <img
+                    src={'//static.synccloud.com/avatars/m/49f0bbd5_1c22_4f37_813c_c2da33d195e6.jpg'}
+                    title={person.fullName} className={`b-avatar ${b}__avatar-img b-avatar_size_m`}/>
+                    <div className={`${b}__selected-avatar-img b-avatar b-avatar_size_m b-avatar_empty_yes`}>
+                        <div className={`${b}__selected-avatar-icon`}>
+                            <div className={'b-icon-sc b-icon-sc_img_person'}></div>
+
+
+                        </div>
+                    </div>
+                </div>
+                    <div className={`${b}__info-wrap`}>
+                        <div className={`${b}__employee-name`}>{person.fullName}</div>
+                        <div className={`${b}__employee-info`}>{person.position}</div>
+                    </div>
+                    <div className={`${b}__person-icon ${b}__person-icon_is-team-member_true`}>
+
+                        <div className={'b-icon-sc b-icon-sc_img_person'}></div>
+
+                    </div>
+                    <div className={`${b}__person-icon ${b}__person-icon_is-team-participant_true`}>
+                        <div className={'b-icon-sc b-icon-sc_img_person'}></div>
+                    </div>
             </div>
         )
     }
